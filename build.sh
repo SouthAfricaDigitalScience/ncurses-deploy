cba438bb0549162a80f0af6545e314b88de5cdcb
Bruce Becker <brucellino@gmail.com>
2017-05-04 14:34:52 +0200

    Preparing for release

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -e
-. /etc/profile.d/modules.sh
-# This is the demo version of a build script for the SAGrid Jenkins instance. Customise it for your needs
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# If you are building from tarball, such as code obtained from sourceforge or github releases, you can use the
-# NAME and VERSION variables defined in the Jenkins job to get the right package for you.
-# Be careful of capitalisation and semantic versioning differences
-# E.g. ApplicationName-v.1.2.3 is different from application-name-1.2.3 and so on.
+. /etc/profile.d/modules.sh
 
 SOURCE_FILE=${NAME}-${VERSION}.tar.gz
 
 # We provide the base module which all jobs need to get their environment on the build slaves
 module add ci
-# Next, a bit of verbose description of the build environment. This is useful when debugging initial builds and you
-# may want to remove it later.
 
-# this tells you the main variables you can use which are set by the ci module
 echo "REPO_DIR is "
 echo ${REPO_DIR}
 echo "SRC_DIR is "
@@ -24,14 +29,8 @@ echo ${WORKSPACE}
 echo "SOFT_DIR is"
 echo ${SOFT_DIR}
 
-
-# In order to get started, we need to ensure that the following directories are available
-
-# Workspace is the "home" directory of jenkins into which the project itself will be created and built.
 mkdir -p ${WORKSPACE}
-# SRC_DIR is the local directory to which all of the source code tarballs are downloaded. We cache them locally.
 mkdir -p ${SRC_DIR}
-# SOFT_DIR is the directory into which the application will be "installed"
 mkdir -p ${SOFT_DIR}
 
 #  Download the source file if it's not available locally.
@@ -57,11 +56,6 @@ fi
 # now unpack it into the workspace
 tar -xvzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
 
-#  generally tarballs will unpack into the NAME-VERSION directory structure. If this is not the case for your application
-#  ie, if it unpacks into a different default directory, either use the relevant tar commands, or change
-#  the next lines
-
-# We will be running configure and make in this directory
 cd ${WORKSPACE}/${NAME}-${VERSION}
 # Note that $SOFT_DIR is used as the target installation directory.
 CPPFLAGS='-P' CFLAGS='-fPIC' ./configure \
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # this should be run after check-build finishes.
 . /etc/profile.d/modules.sh
 echo ${SOFT_DIR}
@@ -21,7 +34,7 @@ CPPFLAGS='-P' CFLAGS='-fPIC' ./configure \
 --prefix=${SOFT_DIR}
 make install
 
-mkdir -p ${LIBRARIES_MODULES}/${NAME}
+mkdir -p ${LIBRARIES}/${NAME}
 
 # Now, create the module file for deployment
 (
@@ -40,7 +53,7 @@ setenv       NCURSES_DIR                 $::env(CVMFS_DIR)/$::env(SITE)/$::env(O
 prepend-path LD_LIBRARY_PATH   $::env(NCURSES_DIR)/lib
 prepend-path PATH                           $::env(NCURSES_DIR)/bin
 MODULE_FILE
-) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
+) > ${LIBRARIES}/${NAME}/${VERSION}
 module purge
 
 module add deploy
